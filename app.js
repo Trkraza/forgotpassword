b8c36a459ffa62552e8150999c95da2bd9269530
Touheed Razaq <trkraza0204@gmail.com>
2023-08-28 23:50:01 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/conn");
+const router = require("./routes/router");
+const cors = require("cors");
+const cookiParser = require("cookie-parser")
+const port = 8009;
+
+// const crypto = require('crypto');
+
+// // Generate a random secret key
+// const secretKey = crypto.randomBytes(32).toString('hex');
+// console.log(secretKey);
+
+
+// app.get("/",(req,res)=>{
+//     res.status(201).json("server created")
+// });
+
+app.use(express.json());
+app.use(cookiParser());
+app.use(cors());
+app.use(router);
+
+
+app.listen(port,()=>{
+    console.log(`server start at port no : ${port}`);
+})
